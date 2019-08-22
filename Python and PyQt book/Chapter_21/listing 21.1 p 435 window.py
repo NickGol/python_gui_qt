1f1d8e82caa3d9ac2ba6ce18721dbda0cec9b434
NickGol <43709923+NickGol@users.noreply.github.com>
2019-08-23 00:56:07 +0300

    21.2

--- Python and PyQt book/Chapter_21/listing 21.1 p 435 window.py
+++ Python and PyQt book/Chapter_21/listing 21.1 p 435 window.py
@@ -1,14 +1,33 @@
 # -*- coding: utf-8 -*-
-from PyQt5 import QtWidgets
-import sys
+from PyQt5 import QtWidgets, QtCore
+import sys, time
 
 app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QWidget()             # Создаем окно
 window.setWindowTitle("Заголовок окна")  # Указываем заголовок
 window.resize(300, 50)                   # Минимальные размеры
+#
+window.setWindowFlags(QtCore.Qt.Window |
+                      QtCore.Qt.FramelessWindowHint |
+                      QtCore.Qt.WindowTitleHint |
+                      QtCore.Qt.WindowStaysOnTopHint)
+window.setWindowOpacity(0.5)
+print(window.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
 window.show()                            # Отображаем окно
+"""print(window.isVisible())
+print(window.isHidden())
 i=5
 print("123")
+time.sleep(1)
+window.hide()
+print(window.isVisible())
+print(window.isHidden())
+time.sleep(1)
+window.setVisible(True)
+print(window.isVisible())
+print(window.isHidden())
+window.setWindowTitle("qwerty123")
+time.sleep(3)"""
 sys.exit(app.exec_())
 print("456")
 i=5
